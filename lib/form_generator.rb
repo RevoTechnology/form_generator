56ca150a767412d1b6400138ea53d36427fb1cd9
Michael Mayorov <mmajorov@gmail.com>
2016-02-19 19:07:18 +0300

    [fix] constant reloading

--- lib/form_generator.rb
+++ lib/form_generator.rb
@@ -3,6 +3,7 @@ require "form_generator/core"
 require "form_generator/form"
 require "form_generator/css_renderer"
 require "form_generator/js_renderer"
+require "form_generator/cream/cream"
 
 # @form = FormGenerator.new(params, request).form
 # if form.nil?
